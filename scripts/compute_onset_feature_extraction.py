d1963b88e301af9136f3e750fa6a755ee39cdd43
wuzhijing <lswzj11@gmail.com>
2019-04-10 00:11:46 -0400

    update

--- scripts/compute_onset_feature_extraction.py
+++ scripts/compute_onset_feature_extraction.py
@@ -155,19 +155,4 @@ print(feat2.count())
 spark.createDataFrame(feat1).coalesce(1).write.option("header", "true").csv("../data/POS_FEAT1.csv")
 spark.createDataFrame(feat2).coalesce(1).write.option("header", "true").csv("../data/POS_FEAT2.csv")
 
-# negative cases
-negative_chart = icu_0.join(chart, ["ICUSTAY_ID"])
-feat_chart_neg = negative_chart.filter(negative_chart.ITEMID.isin(feat_list))
-feat_chart_neg_time = feat_chart_neg.select("ICUSTAY_ID", "CHARTTIME").groupBy("ICUSTAY_ID").agg(Fmin("CHARTTIME")).withColumnRenamed("min(CHARTTIME)", "FEATURE1_TIME")
-feat_chart_neg_time = feat_chart_neg_time.withColumn("FEATURE2_TIME", feat_chart_neg_time.FEATURE1_TIME + expr('INTERVAL 1 HOURS'))
-feat_chart_neg = feat_chart_neg.join(feat_chart_neg_time, ["ICUSTAY_ID"]).rdd
-feat_chart_neg.persist()
-feat1neg = feat_chart_neg.filter(lambda x: (x.FEATURE1_TIME.year, x.FEATURE1_TIME.month, x.FEATURE1_TIME.day, x.FEATURE1_TIME.hour) == (x.CHARTTIME.year, x.CHARTTIME.month, x.CHARTTIME.day, x.CHARTTIME.hour))
-feat2neg = feat_chart_neg.filter(lambda x: (x.FEATURE2_TIME.year, x.FEATURE2_TIME.month, x.FEATURE2_TIME.day, x.FEATURE2_TIME.hour) == (x.CHARTTIME.year, x.CHARTTIME.month, x.CHARTTIME.day, x.CHARTTIME.hour))
-print(feat1neg.count())
-print(feat2neg.count())
-
-spark.createDataFrame(feat1neg).coalesce(1).write.option("header", "true").csv("../data/NEG_FEAT1.csv")
-spark.createDataFrame(feat2neg).coalesce(1).write.option("header", "true").csv("../data/NEG_FEAT2.csv")
-
 spark.stop()
